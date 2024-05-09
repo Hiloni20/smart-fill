7703d2b91b1965c50f1630fc046ab77d0cd783a8
Hiloni20 <158073489+Hiloni20@users.noreply.github.com>
2024-05-08 22:40:34 -0700

    Update st.py

--- pages/st.py
+++ pages/st.py
@@ -1,17 +1,14 @@
 import requests
 import streamlit as st
 
-def preprocess_excel_files(selected_files):
+def preprocess_excel_files(selected_files, username, password):
     if not selected_files:
         st.error("Please select at least one file to preprocess.")
         return
     
     # Define Airflow API endpoint
     airflow_api_url = 'http://localhost:8080/api/v1/dags/preprocess_extract_columns/dagRuns'
-    username = "admin"
-    password = "Hilonib20!"
     # Create authentication headers
-    auth = (username, password)
     headers = {"Content-type": "application/json"}
     payload = {
         "conf": {},
@@ -19,7 +16,7 @@ def preprocess_excel_files(selected_files):
     }
     
     try:
-        response = requests.post(airflow_api_url, headers=headers, auth=auth, json=payload)
+        response = requests.post(airflow_api_url, headers=headers, auth=(username, password), json=payload)
         response.raise_for_status()  # Raise an error for HTTP errors
         st.success("Airflow DAG triggered successfully.")
     except requests.exceptions.ConnectionError as e:
@@ -43,9 +40,12 @@ def main():
         selected_files.append('family_members.xlsx')
     if financial_assets:
         selected_files.append('financial_assets.xlsx')
+    
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
     if st.button('Preprocess selected files'):
-        preprocess_excel_files(selected_files)
+        preprocess_excel_files(selected_files, username, password)
 
 if __name__ == '__main__':
     main()
